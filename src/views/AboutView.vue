e9d346f2c639cf3ffaccdd270e4c59f9f0d54d2c
v833 <2507301541@qq.com>
2025-03-26 23:13:24 +0800

    feat: 添加Tailwind CSS和Arco Design依赖，并初始化相关配置
    本次提交主要引入了Tailwind CSS和Arco Design Vue组件库，并完成了相关配置文件的初始化。具体包括：
    1. 添加了Tailwind CSS及其相关依赖，并在`main.css`中引入。
    2. 添加了Arco Design Vue组件库，并在`main.ts`中注册使用。
    3. 更新了`vite.config.ts`以支持Tailwind CSS。
    4. 移除了旧的`main.css`和`base.css`文件，替换为新的样式配置。
    5. 更新了`tsconfig`文件，关闭了`noImplicitAny`选项以简化开发。
    6. 添加了多个视图组件和布局组件，并更新了路由配置以支持新的页面结构。

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1 class="text-3xl font-bold underline text-blue-700">Hello world!</h1>
+    <p>程序员的梦工厂</p>
+    <a-button type="primary">注册登录</a-button>
+    <a-avatar :style="{ backgroundColor: '#3370ff' }">
+      <icon-user />
+    </a-avatar>
   </div>
 </template>
 
--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <router-view />
+  <p>这是授权登录页面</p>
 </template>
 
 <style scoped></style>
